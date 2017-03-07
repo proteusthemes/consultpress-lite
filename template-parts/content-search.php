8725bf5ee59b848b31d253cfc96b948064aae53a
Marko Prelec <mareprelc@gmail.com>
2017-03-07 13:42:53 +0100

    Fix page search results

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -19,16 +19,18 @@
 
 	<!-- Content Box -->
 	<div class="article__content">
-		<div class="article__meta">
-			<!-- Categories -->
-			<?php if ( has_category() ) : ?>
-				<span class="article__categories"><?php the_category( ' ' ); ?></span>
-			<?php endif; ?>
-			<!-- Author -->
-			<span class="article__author"><i class="fa fa-user" aria-hidden="true"></i><span class="p-author"><?php the_author(); ?></span></span>
-			<!-- Date -->
-			<a class="article__date" href="<?php the_permalink(); ?>"><time class="dt-published" datetime="<?php the_time( 'c' ); ?>"><i class="fa fa-calendar-o" aria-hidden="true"></i> <?php echo get_the_date(); ?></time></a>
-		</div>
+		<?php if ( 'page' !== get_post_type( get_the_ID() ) ) : ?>
+			<div class="article__meta">
+				<!-- Categories -->
+				<?php if ( has_category() ) : ?>
+					<span class="article__categories"><?php the_category( ' ' ); ?></span>
+				<?php endif; ?>
+				<!-- Author -->
+				<span class="article__author"><i class="fa fa-user" aria-hidden="true"></i><span class="p-author"><?php the_author(); ?></span></span>
+				<!-- Date -->
+				<a class="article__date" href="<?php the_permalink(); ?>"><time class="dt-published" datetime="<?php the_time( 'c' ); ?>"><i class="fa fa-calendar-o" aria-hidden="true"></i> <?php echo get_the_date(); ?></time></a>
+			</div>
+		<?php endif; ?>
 		<!-- Content -->
 		<?php the_title( sprintf( '<h2 class="article__title  p-name"><a class="article__title-link  u-url" href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 		<p class="e-content">
