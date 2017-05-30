c9983a0f25b424ea0148ec1d512acfeb1f9cf649
Gregor Capuder <capuderg@gmail.com>
2017-05-30 12:56:26 +0200

    Remove core search markup

--- functions.php
+++ functions.php
@@ -104,11 +104,10 @@ if ( ! function_exists( 'consultpresslite_theme_setup' ) ) {
 		register_nav_menu( 'main-menu', esc_html__( 'Main Menu', 'consultpress-lite' ) );
 
 		/**
-		 * Switch default core markup for search form, comment form, and comments
+		 * Switch default core markup for comment form, and comments
 		 * to output valid HTML5.
 		 */
 		add_theme_support( 'html5', array(
-			'search-form',
 			'comment-form',
 			'comment-list',
 			'gallery',
